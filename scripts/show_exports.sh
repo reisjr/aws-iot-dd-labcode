86d1fa5beb30153836f18394920ef59764a4b2b8
David Reis <reisjr@gmail.com>
2020-08-05 16:31:20 -0300

    Updating to python 3.8

--- scripts/show_exports.sh
+++ scripts/show_exports.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-PROFILE=sandbox
-URL=`aws s3 presign --expires-in 300000 s3://dreis-sandbox-temp/dev-DDQA.json --profile $PROFILE`
+PROFILE=ws02
+#URL=`aws s3 presign --expires-in 300000 s3://dreis-sandbox-temp/dev-DDQA.json --profile $PROFILE`
+URL=`aws s3 presign --expires-in 300000 s3://dreis-ws02-temp/dev-RPTG.cfg --profile $PROFILE`
 PORT=8080
 
 echo "export AWS_PROFILE=$PROFILE"
